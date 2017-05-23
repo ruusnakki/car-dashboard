0cf18a14c2b65adddb80a35c8b40121c3dce0314
Iwan Winoto <iwinoto@au1.ibm.com>
2017-05-23 19:47:28 +1000

    the real deploy script

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,6 +1,15 @@
-Script started on Tue May 23 17:40:25 2017
-[?1034hbash-3.2$ 
-bash-3.2$ eixt[K[K[Kxit
-exit
+#!/bin/bash
+# Create services
+# conversation
+cf create-service conversation free my-conversation-service
+# speech-to-text
+cf create-service speech_to_text standard speech-to-text-service
+# text-to-speech
+cf create-service text_to_speech standard text-to-speech-service
 
-Script done on Tue May 23 17:40:46 2017
+# Push app
+export CF_APP_NAME="staging-$CF_APP"
+cf push "${CF_APP_NAME}" -n ${CF_APP_NAME}
+export APP_URL=http://$(cf app $CF_APP_NAME | grep urls: | awk '{print $2}')
+# View logs
+#cf logs "${CF_APP_NAME}" --recent
