53fdd88b42bd17cc149001025dc65e6965704e07
Iwan Winoto <iwinoto@au1.ibm.com>
2017-08-03 22:46:47 +1000

    Login in to docker with bx cr

--- install-bx.sh
+++ install-bx.sh
@@ -2,8 +2,11 @@
 DOCKER_VER=17.06.0
 # Install Docker
 # ref: https://docs.docker.com/engine/installation/linux/docker-ce/binaries/
-wget --output-document=tmp/docker/docker-$DOCKER_VER-ce.tgz https://download.docker.com/mac/static/stable/x86_64/docker-$DOCKER_VER-ce.tgz
-tar -xzf tmp/docker/docker-$DOCKER_VER-ce.tgz
+wget --output-document=/tmp/docker-$DOCKER_VER-ce.tgz https://download.docker.com/mac/static/stable/x86_64/docker-$DOCKER_VER-ce.tgz
+ls -l /tmp/
+tar -xzf /tmp/docker-$DOCKER_VER-ce.tgz
+ls -l
+ls -l docker/
 export PATH=$PATH:docker
 
 echo "Download Bluemix CLI"
