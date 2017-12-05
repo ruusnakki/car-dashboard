eced5ac922469a3795910c12646fa8d211adcac1
Iwan Winoto <iwinoto@au1.ibm.com>
2017-12-06 06:11:01 +1100

    CF command

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
+CF_CMD="bx cf"
+echo "cf command is " $CF_CMD
 # Create services
 # conversation
-cf create-service conversation free my-conversation-service
+$CF_CMD create-service conversation free my-conversation-service
 # speech-to-text
-cf create-service speech_to_text standard speech-to-text-service
+$CF_CMD create-service speech_to_text standard speech-to-text-service
 # text-to-speech
-cf create-service text_to_speech standard text-to-speech-service
+$CF_CMD create-service text_to_speech standard text-to-speech-service
 
 # Push app
 HOST=$1
-if ! cf app $CF_APP; then
-  cf push $CF_APP -n $HOST
+if ! $CF_CMD app $CF_APP; then
+  $CF_CMD push $CF_APP -n $HOST
 else
   OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
   rollback() {
     set +e
-    if cf app $OLD_CF_APP; then
-      cf logs $CF_APP --recent
-      cf delete $CF_APP -f
-      cf rename $OLD_CF_APP $CF_APP
+    if $CF_CMD app $OLD_CF_APP; then
+      $CF_CMD logs $CF_APP --recent
+      $CF_CMD delete $CF_APP -f
+      $CF_CMD rename $OLD_CF_APP $CF_APP
     fi
     exit 1
   }
   set -e
   trap rollback ERR
-  cf rename $CF_APP $OLD_CF_APP
-  cf push $CF_APP -n $HOST
-  cf delete $OLD_CF_APP -f
+  $CF_CMD rename $CF_APP $OLD_CF_APP
+  $CF_CMD push $CF_APP -n $HOST
+  $CF_CMD delete $OLD_CF_APP -f
 fi
 # Export app name and URL for use in later Pipeline jobs
 export CF_APP_NAME="$CF_APP"
-export APP_URL=http://$(cf app $CF_APP_NAME | grep urls: | awk '{print $2}')
+export APP_URL=http://$($CF_CMD app $CF_APP_NAME | grep urls: | awk '{print $2}')
 # View logs
-#cf logs "${CF_APP}" --recent
+#$CF_CMD logs "${CF_APP}" --recent
